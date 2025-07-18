cc6a3152d793d67f0336a529e4e55c3cb6416ce6
Arush Khare <56801812+ArushKhare@users.noreply.github.com>
2025-07-18 13:41:01 -0700

    gemini added

--- flask_website/app.py
+++ flask_website/app.py
@@ -5,10 +5,13 @@ import whisper
 import pymupdf4llm as p4
 import nltk
 from transformers import pipeline
+import google.generativeai as genai
+import base64
+from lib_funcs import *
 
 nltk.download('punkt')
 nltk.download('punkt_tab')
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+#summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 app = Flask(__name__)
 app.secret_key = "racetrack_playa"
@@ -17,15 +20,37 @@ model = whisper.load_model("base")
 """
 GENERAL FUNCTIONS
 """
+genai.configure(api_key=get_api_key("Gemini"))
+summary_model = genai.GenerativeModel("gemini-2.0-flash")
 
 def summarize(text):
+    return summary_model.generate_content(f"Summarize this text: {text}").text
+
+def summarize_ocr(img_path):
+    f_type = img_path.split(".")[-1]
+    with open(img_path, "rb") as image_file:
+        encoded_image_string = base64.b64encode(image_file.read()).decode("utf-8")
+    ocr_response = summary_model.generate_content(
+        [
+            {"text": "Tell me what this image says and nothing else"},
+                {"inline_data": {
+                    "mime_type": f"image/{f_type}", #change to image/png,webp,etc. as needed
+                    "data": encoded_image_string
+                }
+            }
+        ]
+    )
+    return summarize(ocr_response.text)
+    
+
+"""def summarize(text):
     summarized_chunks = []
     for i in range(0, len(text), 1024):
         summarized_chunks.append(summarizer(text[i: min(i + 1024, len(text))], max_length=130, min_length=30, do_sample=False)[0]['summary_text'])
     ret = ""
     for chunk in summarized_chunks:
         ret += chunk
-    return ret
+    return ret"""
 
 def get_doc_summary(name):
     reader = p4.LlamaMarkdownReader()
@@ -56,7 +81,6 @@ def get_doc_summary(name):
         t += line + "\n"
 
     return summarize(t)
-
 
 """
 WEBSITE FUNCTIONS
@@ -142,6 +166,32 @@ def summarize_doc():
             os.remove(tmp_path)
     
     return render_template("document.html")
+
+@app.route("/summarize-writing", methods=["POST", "GET"])
+def summarize_writing():
+    if (request.method == "POST"):
+
+        file_upload = request.files.get("file")
+
+        if not file_upload:
+            return render_template("handwrite.html", summary="Please select a file!")
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file_upload.filename)[1]) as tmp:
+            file_upload.save(tmp.name)
+            tmp_path = tmp.name
+        
+        try:
+            result = summarize_ocr(tmp_path)
+
+            template = f"<h2 class='content'>Text Summary:</h2> \
+            <p class='content'>{result}</p>"
+
+            return render_template('handwrite.html') + template
+    
+        finally:
+            os.remove(tmp_path)
+    
+    return render_template("handwrite.html")
 
 if __name__=='__main__':
     app.run(debug=True)
